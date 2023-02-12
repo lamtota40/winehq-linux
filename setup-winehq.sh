9d06e77914c202ee9fe4d5e89ea7706aa704d32e
lamtota40 <lamtota40@gmail.com>
2023-02-12 19:05:28 +0700

    Update setup-winehq.sh

--- setup-winehq.sh
+++ setup-winehq.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo dpkg --add-architecture i386
 wget -qO- dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
